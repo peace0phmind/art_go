4b5cfc1e22089166ebd44a587e11d72fea397f7d
peace0phmind <peace0phmind@gmail.com>
2022-01-27 14:32:59 +0800

    add output to csv

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/peace0phmind/art_go
 
 go 1.15
+
+require github.com/gocarina/gocsv v0.0.0-20211203214250-4735fba0c1d9
--- main/main.go
+++ main/main.go
@@ -2,9 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/gocarina/gocsv"
 	"github.com/peace0phmind/art_go"
+	"os"
 	"time"
 )
+
+type Csv struct { // Our example struct, you can use "-" to ignore a field
+	Id  string  `csv:"date"`
+	C1  float64 `csv:"c1"`
+	C2  float64 `csv:"c2"`
+	C3  float64 `csv:"c3"`
+	C4  float64 `csv:"c4"`
+	C5  float64 `csv:"c5"`
+	C6  float64 `csv:"c6"`
+	C7  float64 `csv:"c7"`
+	C8  float64 `csv:"c8"`
+	C9  float64 `csv:"c9"`
+	C10 float64 `csv:"c10"`
+	C11 float64 `csv:"c11"`
+	C12 float64 `csv:"c12"`
+	C13 float64 `csv:"c13"`
+	C14 float64 `csv:"c14"`
+	C15 float64 `csv:"c15"`
+	C16 float64 `csv:"c16"`
+}
+
+const cannel_count = 16
 
 func main() {
 	art, err := art_go.NewArt(0)
@@ -18,11 +42,11 @@ func main() {
 		// 时间参数
 		param.SampleMode = art_go.SAMPLE_MODE_CONTINUOUS
 		param.SamplePerChannel = 1000
-		param.SampleRate = 2000
+		param.SampleRate = 1000
 		param.SampleClockSource = art_go.SAMPLE_CLOCK_SOURCE_LOCAL
 		param.ExtSampleClockEdge = art_go.Ext_SAMPLE_CLOCK_EDGE_RISING
 
-		for i := 0; i < 4; i++ {
+		for i := 0; i < cannel_count; i++ {
 			ch := art_go.AIChannelParam{}
 			ch.Channel = i
 			ch.SampleRange = art_go.SAMPLE_RANGE_N10_P10V
@@ -40,17 +64,51 @@ func main() {
 				if err = task.SendSoftTrig(); err != nil {
 					println(fmt.Sprintf("send soft trig err: %v", err))
 				} else {
-					buf := make([]float64, 4*param.SamplePerChannel)
-					for i := 0; i < 1000; i++ {
+					var csvs []Csv
+					buf := make([]float64, cannel_count*param.SamplePerChannel)
+					for i := 0; i < 20; i++ {
 						count, err := task.ReadAnalog(&buf, 10, art_go.FILL_MODE_GroupByScanNumber)
 						println(fmt.Sprintf("%s get count: %v", time.Now().Format("0405.999999999"), count))
 						if err != nil {
 							println(fmt.Sprintf("read analog error: %v", err))
 						} else {
-							println(fmt.Sprintf("%v, %v, %v, %v", buf[0], buf[1], buf[2], buf[3]))
-							println(fmt.Sprintf("%v, %v, %v, %v", buf[4], buf[5], buf[6], buf[7]))
+							dd := time.Now()
+
+							for j := 0; j < param.SamplePerChannel; j++ {
+								csvs = append(csvs, Csv{
+									Id:  fmt.Sprintf("%s%03d", dd.Format("20060201150405"), j),
+									C1:  buf[cannel_count*j+0],
+									C2:  buf[cannel_count*j+1],
+									C3:  buf[cannel_count*j+2],
+									C4:  buf[cannel_count*j+3],
+									C5:  buf[cannel_count*j+4],
+									C6:  buf[cannel_count*j+5],
+									C7:  buf[cannel_count*j+6],
+									C8:  buf[cannel_count*j+7],
+									C9:  buf[cannel_count*j+8],
+									C10: buf[cannel_count*j+9],
+									C11: buf[cannel_count*j+10],
+									C12: buf[cannel_count*j+11],
+									C13: buf[cannel_count*j+12],
+									C14: buf[cannel_count*j+13],
+									C15: buf[cannel_count*j+14],
+									C16: buf[cannel_count*j+15],
+								})
+							}
 						}
 					}
+
+					csvFile, err := os.OpenFile("clients.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+					if err != nil {
+						panic(err)
+					}
+					defer csvFile.Close()
+
+					err = gocsv.MarshalFile(&csvs, csvFile) // Use this to save the CSV back to the file
+					if err != nil {
+						panic(err)
+					}
+
 				}
 
 				task.Stop()
