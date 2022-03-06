61dbeddc817b59feea8bea1781c4f4163189a05c
peace0phmind <peace0phmind@gmail.com>
2022-03-06 14:12:06 +0800

    add chan to save data

--- main/main.go
+++ main/main.go
@@ -7,36 +7,51 @@ import (
 	"github.com/influxdata/influxdb-client-go/v2/api/http"
 	"github.com/peace0phmind/art_go"
 	"strconv"
+	"sync"
 	"time"
 )
 
-type Csv struct { // Our example struct, you can use "-" to ignore a field
-	Id  string  `csv:"date"`
-	C1  float64 `csv:"c1"`
-	C2  float64 `csv:"c2"`
-	C3  float64 `csv:"c3"`
-	C4  float64 `csv:"c4"`
-	C5  float64 `csv:"c5"`
-	C6  float64 `csv:"c6"`
-	C7  float64 `csv:"c7"`
-	C8  float64 `csv:"c8"`
-	C9  float64 `csv:"c9"`
-	C10 float64 `csv:"c10"`
-	C11 float64 `csv:"c11"`
-	C12 float64 `csv:"c12"`
-	C13 float64 `csv:"c13"`
-	C14 float64 `csv:"c14"`
-	C15 float64 `csv:"c15"`
-	C16 float64 `csv:"c16"`
+type AmpereValue struct {
+	Channel int
+	Value   float64
+	Time    time.Time
+}
+
+var ampereValuePool sync.Pool
+
+func getAmpereValue() *AmpereValue {
+	v := ampereValuePool.Get()
+	if v == nil {
+		v = &AmpereValue{}
+	}
+
+	return v.(*AmpereValue)
+}
+
+func putAmpereValue(v *AmpereValue) {
+	ampereValuePool.Put(v)
 }
 
 const channel_count = 16
-const channel_count_for_save = 15
+const channel_count_for_save = 16
 
 func printInfluxdbError(writeAPI api.WriteAPI) {
 	for e := range writeAPI.Errors() {
 		println(e)
 	}
+}
+
+func saveAmpereToInfluxdb(vvc chan *AmpereValue, writeAPI api.WriteAPI) {
+	for v := range vvc {
+		p := influxdb2.NewPointWithMeasurement("Ampere").
+			AddTag("channel", strconv.Itoa(v.Channel+1)).
+			AddField("value", v.Value).
+			SetTime(v.Time)
+		writeAPI.WritePoint(p)
+		putAmpereValue(v)
+	}
+
+	writeAPI.Flush()
 }
 
 func main() {
@@ -58,6 +73,10 @@ func main() {
 			println(fmt.Sprintf("do retry: %s, err: %v", batch, error))
 			return true
 		})
+
+		// init chan task
+		ampereValueChan := make(chan *AmpereValue, 1000*32*60)
+		go saveAmpereToInfluxdb(ampereValueChan, writeAPI)
 
 		param := &art_go.AITaskParam{}
 		param.SampleSignal = art_go.AI_SAMPLE_SIGNAL_AI
@@ -102,50 +121,15 @@ func main() {
 
 							for j := 0; j < param.SamplePerChannel; j++ {
 								for c := 0; c < channel_count_for_save; c++ {
-									p := influxdb2.NewPointWithMeasurement("Ampere").
-										AddTag("channel", strconv.Itoa(c+1)).
-										AddField("value", buf[channel_count*j+c]).
-										SetTime(time.Unix(dd.Unix(), int64(1000000*j)))
-									// write asynchronously
-									writeAPI.WritePoint(p)
+									v := getAmpereValue()
+									v.Channel = c
+									v.Value = buf[channel_count*j+c]
+									v.Time = time.Unix(dd.Unix(), int64(1000000*j))
+									ampereValueChan <- v
 								}
-
-								//csvs = append(csvs, Csv{
-								//	Id:  fmt.Sprintf("%s%03d", dd.Format("20060201150405"), j),
-								//	C1:  buf[cannel_count*j+0],
-								//	C2:  buf[cannel_count*j+1],
-								//	C3:  buf[cannel_count*j+2],
-								//	C4:  buf[cannel_count*j+3],
-								//	C5:  buf[cannel_count*j+4],
-								//	C6:  buf[cannel_count*j+5],
-								//	C7:  buf[cannel_count*j+6],
-								//	C8:  buf[cannel_count*j+7],
-								//	C9:  buf[cannel_count*j+8],
-								//	C10: buf[cannel_count*j+9],
-								//	C11: buf[cannel_count*j+10],
-								//	C12: buf[cannel_count*j+11],
-								//	C13: buf[cannel_count*j+12],
-								//	C14: buf[cannel_count*j+13],
-								//	C15: buf[cannel_count*j+14],
-								//	C16: buf[cannel_count*j+15],
-								//})
 							}
-
-							writeAPI.Flush()
 						}
 					}
-
-					//csvFile, err := os.OpenFile("clients.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
-					//if err != nil {
-					//	panic(err)
-					//}
-					//defer csvFile.Close()
-					//
-					//err = gocsv.MarshalFile(&csvs, csvFile) // Use this to save the CSV back to the file
-					//if err != nil {
-					//	panic(err)
-					//}
-
 				}
 
 				task.Stop()
